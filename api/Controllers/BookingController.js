bf3c1f2278c24ec232804460f9cc0a5a0e5ec7f3
ManasSin <maltisadan12@gmail.com>
2024-09-27 00:27:41 +0530

    REFACTOR: BookingController Booking.route :
    - Restructure the booking creation route to include additional functionality for creating a booking, updating the hotel branch, and sending email notifications.
    - Add a new route for retrieving a booking based on the booking ID and user ID.
    - Implement authorization checks to ensure that only the user who made the booking can retrieve it.
    - Handle various scenarios such as unpaid bookings, paid bookings, cancelled bookings, and generate appropriate responses.

--- api/Controllers/BookingController.js
+++ api/Controllers/BookingController.js
@@ -1,15 +1,30 @@
 import express from "express";
-import mongoose, { now } from "mongoose";
+import mongoose from "mongoose";
 import { asyncHandler } from "../Service/asyncHandler.js";
-import fs from "fs";
-import path from "path";
 import HotelBranch from "../Modal/Hotels/HotelBranchSchema.js";
 import Booking from "../Modal/Hotels/BookingSchema.js";
+import Room from "../Modal/Rooms/RoomSchema.js";
 // import PDFDocument from "pdfkit";
+
+/* 
+  functionality: 
+  1. create booking for a hotel, update the hotelBranch
+    1.1. if the booking is created and not paid within 30 mins send them an email,
+    1.2. if the booking is created and still not paid within 60 mins, set isCancelled to true on the booking
+    1.3. if the booking is created and paid within 60 mins or as soon as they pay, update the hotelBranch and send them an email
+
+  2. get booking using the booking id and user id
+    2.1. get booking, check if the booking belongs to the user,
+      2.1.1 if the booking exists and is unpaid, return booking with time remaining to make payment, and assignedStaff details.
+    2.2. check if the booking is already paid, if yes then return booking with the assignedStaff details, invoice, and location of the booked hotelBranch. 
+    2.3. check if the booking is cancelled, if yes then return just the booking details, refund status, and payment mode.
+    2.3. return booking
+
+*/
 
 export const createBooking = asyncHandler(async (req, res) => {
   const userID = req.user._id;
-  const { user, hotelBranchId } = req.query;
+  const { userId: user, hotelBranchId, roomId } = req.query;
   const {
     checkInDate,
     checkOutDate,
@@ -39,124 +54,233 @@ export const createBooking = asyncHandler(async (req, res) => {
     });
   }
 
-  if (
-    !checkInDate ||
-    !checkOutDate ||
-    !guests ||
-    typeof guests !== "object" ||
-    (guests.children !== undefined &&
-      (typeof guests.children !== "number" || guests.children < 0)) ||
-    (guests.infants !== undefined &&
-      (typeof guests.infants !== "number" || guests.infants < 0)) ||
-    !paymentMethod ||
-    typeof paymentMethod !== "string" ||
-    !["credit card", "debit card", "paypal", "online"].includes(
-      paymentMethod
-    ) ||
-    !listingPrice ||
-    typeof listingPrice !== "number" ||
-    listingPrice < 0 ||
-    !discount ||
-    typeof discount !== "number" ||
-    discount < 0 ||
-    !(serviceCharge || typeof serviceCharge === "number") ||
-    !(tax || typeof tax === "number") ||
-    !specialRequest ||
-    typeof specialRequest !== "string" ||
-    specialRequest.trim().length < 1
-  ) {
-    return res
-      .status(401)
-      .send("The fields are not valid. Please check and try again.");
-  }
-
-  // get the serviceCharge, tax, totalAmount from the body, if not provided then take from the hotel details
-  if (!serviceCharge || !tax) {
-    // make serviceCharge  = 16 % of the listingPrice
-    serviceCharge = listingPrice * 0.16;
-    // make tax = 8 % of the listingPrice
-    tax = listingPrice * 0.08;
-  }
-
-  // make totalAmount = listingPrice + serviceCharge + tax
-  const totalAmount = listingPrice + serviceCharge + tax;
-
-  const slugName = `${hotelBranchId.slice(18)}-${user.slice(18)}-${checkInDate
-    .toString()
-    .slice(0, 10)}-${checkOutDate
-    .toString()
-    .slice(0, 10)}-${new Date().setMilliseconds(0)}`;
-
-  // console.log({ slugName: slugName });
-
-  const bookingExists = await Booking.findOne({
-    slugName: slugName,
-  });
-  if (bookingExists) {
-    return res.status(400).json({
-      status: "Error",
-      message: "Booking can't be created",
-      error: "Booking already exists.",
-    });
-  }
-
-  const booking = new Booking({
-    user: new mongoose.Types.ObjectId(String(user)),
-    hotelBranch: new mongoose.Types.ObjectId(String(hotelBranchId)),
-    checkInDate,
-    checkOutDate,
-    guests,
-    specialRequest,
-    paymentMethod,
-    listingPrice,
-    discount,
-    serviceCharge,
-    tax,
-    totalAmount,
-    slugName,
-  });
-
-  const data = await booking.save();
-  res.status(201).send({
-    status: "OK",
-    message: "Booking created successfully",
-    booking: data,
-  });
-});
-
-export const getBooking = asyncHandler(async (req, res) => {
-  const userId = req.user._id;
-
-  // trigger this when a successful booking is made
   try {
-    // Generate a unique invoice number
-    booking.invoiceNumber = `INV-${Date.now()}`;
+    if (
+      !checkInDate ||
+      !checkOutDate ||
+      !guests ||
+      typeof guests !== "object" ||
+      (guests.children !== undefined &&
+        (typeof guests.children !== "number" || guests.children < 0)) ||
+      (guests.infants !== undefined &&
+        (typeof guests.infants !== "number" || guests.infants < 0)) ||
+      !paymentMethod ||
+      typeof paymentMethod !== "string" ||
+      !["credit card", "debit card", "paypal", "online"].includes(
+        paymentMethod
+      ) ||
+      !listingPrice ||
+      typeof listingPrice !== "number" ||
+      listingPrice < 0 ||
+      !discount ||
+      typeof discount !== "number" ||
+      discount < 0 ||
+      !(serviceCharge || typeof serviceCharge === "number") ||
+      !(tax || typeof tax === "number") ||
+      !specialRequest ||
+      typeof specialRequest !== "string" ||
+      specialRequest.trim().length < 1
+    ) {
+      return res
+        .status(401)
+        .send("The fields are not valid. Please check and try again.");
+    }
 
-    // Save the booking to the database
-    await booking.save();
+    // get the serviceCharge, tax, totalAmount from the body, if not provided then take from the hotel details
+    if (!serviceCharge || !tax) {
+      // make serviceCharge  = 16 % of the listingPrice
+      serviceCharge = listingPrice * 0.16;
+      // make tax = 8 % of the listingPrice
+      tax = listingPrice * 0.08;
+    }
 
-    // Generate the PDF invoice
-    const outputPath = path.join(
-      __dirname,
-      "invoices",
-      `${booking.invoiceNumber}.pdf`
-    );
-    generateInvoice(booking, outputPath);
+    // make totalAmount = listingPrice + serviceCharge + tax
+    const totalAmount = listingPrice + serviceCharge + tax;
 
-    res.status(201).json({
+    const slugName = `${hotelBranchId.slice(18)}-${user.slice(18)}-${checkInDate
+      .toString()
+      .slice(0, 10)}-${checkOutDate.toString().slice(0, 10)}-${new Date()
+      .toJSON()
+      .slice(0, 10)}`.toLowerCase();
+
+    // console.log({ slugName: slugName });
+
+    const bookingExists = await Booking.findOne({
+      slugName: slugName,
+    });
+    if (bookingExists) {
+      return res.status(400).json({
+        status: "Error",
+        message: `Booking already exists. ${bookingExists.checkInDate.toDateString()}-${bookingExists.checkOutDate.toDateString()} for ${
+          bookingExists.guests
+        }`,
+        error: "Booking already exists.",
+      });
+    }
+
+    const booking = new Booking({
+      user: new mongoose.Types.ObjectId(String(user)),
+      hotelBranch: new mongoose.Types.ObjectId(String(hotelBranchId)),
+      roomId: new mongoose.Types.ObjectId(String(roomId)),
+      checkInDate,
+      checkOutDate,
+      guests,
+      specialRequest,
+      paymentMethod,
+      listingPrice,
+      discount,
+      serviceCharge,
+      tax,
+      totalAmount,
+      slugName,
+    });
+
+    let bookings, hotelBranches, rooms;
+    // functionality list point number 1
+    await Promise.all([
+      (bookings = booking.save()),
+      (hotelBranches = HotelBranch.findOneAndUpdate(
+        { _id: hotelBranchId },
+        { $inc: { bookingCount: 1 }, $push: { bookings: booking._id } }
+      )),
+      (rooms = Room.findOneAndUpdate(
+        { _id: roomId },
+        { $inc: { numberOfBookingsMade: 1 }, $push: { bookings: booking._id } }
+      )),
+      // user.updateOne({ $push: { bookings: booking._id } }),
+    ]);
+    res.status(201).send({
       status: "OK",
-      message: "Booking created and invoice generated",
-      booking: booking,
+      message: "Booking created successfully",
+      booking: { bookings, hotelBranches, rooms },
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       status: "Error",
-      message: "Error creating booking",
+      message: "Booking can't be created",
       error: error.message,
     });
   }
 });
 
+export const getBooking = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const { userId: user, bookingId } = req.query;
+
+  if (userId.equals(user) === false)
+    return res.status(401).json({
+      status: "Error",
+      message: "Unauthorized",
+      error: "Unauthorized",
+    });
+
+  try {
+    const booking = await Booking.findOne({
+      _id: bookingId,
+      user: user,
+      isCancelled: { $ne: true },
+    }).populate("user hotelBranch roomId", { strictPopulate: false });
+    if (!booking) {
+      return res.status(404).json({
+        status: "Error",
+        message: "Booking not found",
+        error: "Booking not found",
+      });
+    }
+
+    if (booking.isCancelled) {
+      return res.status(404).json({
+        status: "Error",
+        message:
+          "Booking cancelled on " +
+          booking.isCancelled?.cancellationDate.toDateString(),
+        error: "Booking cancelled by " + booking.isCancelled.deletedBy,
+      });
+    }
+
+    const accordingToPaymentStatus =
+      booking.paymentStatus === "paid"
+        ? { connectStaff: booking.hotelBranch.assignedStaff }
+        : {
+            // convert the booking.createdAt to a Date object, and add 60 minutes to it, then convert it back to a timestamp, subtracting the current timestamp, and convert to minutes and decorate with "mins" or "hrs" (remove the decimal part)
+            paymentTimeLeft:
+              (new Date(booking.createdAt).getTime() +
+                60 * 60 * 1000 -
+                Date.now()) /
+                (60 * 1000) <=
+              60
+                ? `${Math.floor(
+                    (new Date(booking.createdAt).getTime() +
+                      60 * 60 * 1000 -
+                      Date.now()) /
+                      (60 * 1000) /
+                      60
+                  )} hrs`
+                : `${Math.floor(
+                    (new Date(booking.createdAt).getTime() +
+                      60 * 60 * 1000 -
+                      Date.now()) /
+                      (60 * 1000)
+                  )} mins`,
+          };
+
+    // console.log(
+    //   accordingToPaymentStatus,
+    //   new Date(booking.createdAt),
+    //   Date.now()
+    // );
+    let returnData = {
+      bookingId: booking._id,
+      paymentStatus: booking.paymentStatus,
+      guests: booking.guests,
+      totalAmount: booking.totalAmount,
+      specialRequest: booking.specialRequest,
+      paymentMethod: booking.paymentMethod,
+      invoice:
+        booking.invoice !== null ? booking.invoice : "No Invoice generated",
+      ...accordingToPaymentStatus,
+      checkInDate: booking.checkInDate.toISOString().slice(0, 10),
+      checkOutDate: booking.checkOutDate.toISOString().slice(0, 10),
+      bookedBy: booking.user.name,
+      hotelName: booking.hotelBranch.name,
+      roomNumber: booking.roomId?.roomNumber ? booking.roomId.roomNumber : "NA",
+      roomType: booking.roomId?.roomType ? booking.roomId.roomType : "NA",
+      roomPrice: booking.roomId?.price ? booking.roomId.price : "NA",
+    };
+
+    res.status(200).json({
+      status: "OK",
+      message: "Booking found successfully",
+      booking: returnData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      message: "server error while fetching booking",
+      error: error.message,
+    });
+  }
+});
+
+// Generate a unique invoice number
+// booking.invoiceNumber = `INV-${Date.now()}`;
+
+// Save the booking to the database
+// await booking.save();
+
+// Generate the PDF invoice
+// const outputPath = path.join(
+//   __dirname,
+//   "invoices",
+//   `${booking.invoiceNumber}.pdf`
+// );
+// generateInvoice(booking, outputPath);
+
+// res.status(201).json({
+//   status: "OK",
+//   message: "Booking created and invoice generated",
+//   booking: booking,
+// });
 // const generateInvoice = (booking, outputPath) => {
 //   const doc = new PDFDocument();
 
--- api/Routes/Booking/Booking.route.js
+++ api/Routes/Booking/Booking.route.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { isLoggedIn } from "../../Middlewares/isLoggedIn.js";
-import { createBooking } from "../../Controllers/BookingController.js";
+import {
+  createBooking,
+  getBooking,
+} from "../../Controllers/BookingController.js";
 
 export const router = Router();
 
 router.post("/create", isLoggedIn, createBooking);
+
+router.route("/").get(isLoggedIn, getBooking);
